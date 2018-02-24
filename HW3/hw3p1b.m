435256339c57ed515fe98541da22a51f2e1f68a9
ZhihaoAi <zai@hawk.iit.edu>
2018-02-24 11:49:01 -0600

    Finish p1

--- HW3/hw3p1b.m
+++ HW3/hw3p1b.m
@@ -1,6 +1,6 @@
 A = [10 -2 -1; -2 10 -1; -1 -2 5]; b = [11; -25; 18];
-d = diag(diag(A));
-M = -inv(d)*(A-d); g = inv(d)*b;
+l = tril(A); u = triu(A,1);
+M = -inv(l)*u; g = inv(l)*b;
 x = [1; -2; 3]; xk = [0; 0; 0]; xk1 = M*xk + g; tol = max(abs(xk1-xk)); k = 1;
 
 while tol > 5*10^-3
